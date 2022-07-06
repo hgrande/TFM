22ce04c265e37b72ae314f0c7bc21ec3b8c5151f
hgrande <52891798+hgrande@users.noreply.github.com>
2022-07-06 19:19:45 +0200

    Add files via upload

---
+++ TablasVerdad.py
@@ -0,0 +1,28 @@
+import numpy as np #Numpy(Numerical Python) es una librería que nos permite trabajar con arrays. 
+#Generador de tablas de verdad
+#Preguntamos al usuario el número de variables binarias que tenemos en el problema
+numero_variables =int(input("Por favor, introduzca el número de variables binarias que tiene el problema:"))
+
+#El número de filas de la tabla de verdad será 2 elevado al número de variables.
+filas = int(2**numero_variables)
+#El número de columnas será el número de variables
+#La dimensión de la matriz será
+#Generamos los valores de cada columna
+mitad= int(filas/2)
+
+#Especificamos el tipo de dato que queremos en nuestra matriz:entero
+#Creamos matriz ceros. 
+arr = np.zeros((filas,numero_variables),dtype='i') 
+#Añadimos unos 
+for j in range(numero_variables):
+    numero_unos = int(2**(numero_variables-j)/2)
+    
+    for i in range(filas):
+   
+        if (arr[i-numero_unos,j]==0):
+
+            for l in range(numero_unos):
+                arr[i,j]=1
+                
+
+print(arr)
